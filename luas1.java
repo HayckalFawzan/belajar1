210905beb73ea018bbcd6f7c736091e5e2815ff2
HayckalFawzan <skiesheaven@gmail.com>
2017-01-27 02:30:19 +0700

    Add files via upload

---
+++ luas1.java
@@ -0,0 +1,14 @@
+class luas1 {
+
+	public static int cariLuasPersegiPanjang(int panjang, int lebar) {
+		return panjang * lebar;
+	}
+
+	public static int cariLuasSegitiga(int alas, int tinggi) {
+		return (alas * tinggi) / 2;
+	}
+
+	public static void main (String[] args) {
+
+	}
+}
